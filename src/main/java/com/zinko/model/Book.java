16ebcdc5d06618ca63e397900204f6dd0a4277bf
VladZinko <zinko1996@yandex.ru>
2024-04-19 23:40:54 +0300

    create entity class Book

---
+++ src/main/java/com/zinko/model/Book.java
@@ -0,0 +1,70 @@
+package com.zinko.model;
+
+
+import java.time.LocalDate;
+
+public class Book {
+
+    private Long id;
+
+    private String author;
+
+    private String title;
+
+    private String isbn;
+
+    private LocalDate publicationDate;
+
+    public Book() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public LocalDate getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(LocalDate publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}

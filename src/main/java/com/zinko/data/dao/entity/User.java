12a74794466b3625a285d9930fa7457e26dcd126
VladZinko <zinko1996@yandex.ru>
2024-04-25 01:04:25 +0300

    created new entity class User, and enum class Role

---
+++ src/main/java/com/zinko/data/dao/entity/User.java
@@ -0,0 +1,11 @@
+package com.zinko.data.dao.entity;
+
+import com.zinko.data.dao.entity.enums.Role;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private char[] password;
+    private Role role;
+}

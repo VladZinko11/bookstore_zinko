1023a0adc8a8134e83f012b72971a6350823aeed
VladZinko <zinko1996@yandex.ru>
2024-04-24 16:04:08 +0300

    modified class Main and ConectionContext
    Fixed a problem with Russian encodings and an error in connecting to a remote database server

--- src/main/java/com/zinko/Main.java
+++ src/main/java/com/zinko/Main.java
@@ -24,34 +24,34 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         boolean execution = true;
         System.out.println("""
-                Введите команду:\s
-                     all - для получения каталога всех книг
-                     get - для получения книги по id\s
-                     delete - для удадения книги по id
-                     create - для создания и добавления книги\s
-                     exit - для завершения работы с каталогом""");
+                Enter the command:\s
+                     all - to get a catalog of all books
+                     get - to get books by id\s
+                     delete - to delete a book by id
+                     create - to create and add book\s
+                     exit - to finish work with the catalog""");
         while (execution) {
             String command = scanner.nextLine();
             switch (command) {
                 case "all" -> bookServiceImpl.findAll().forEach(System.out::println);
                 case "get" -> {
-                    System.out.println("Введите id: ");
+                    System.out.println("Enter id: ");
                     String id = scanner.nextLine();
                     System.out.println(bookServiceImpl.findById(Long.parseLong(id)));
                 }
                 case "delete" -> {
-                    System.out.println("Введите id: ");
+                    System.out.println("Enter id: ");
                     String id = scanner.nextLine();
                     System.out.println(bookServiceImpl.delete(Long.parseLong(id)));
                 }
                 case "create" -> {
-                    System.out.println("Введите автора: ");
+                    System.out.println("Enter author: ");
                     String author = scanner.nextLine();
-                    System.out.println("Введите название: ");
+                    System.out.println("Enter title: ");
                     String title = scanner.nextLine();
-                    System.out.println("Введите isbn: ");
+                    System.out.println("Enter isbn: ");
                     String isbn = scanner.nextLine();
-                    System.out.println("Введите год публикации: ");
+                    System.out.println("Enter publication year: ");
                     int date = scanner.nextInt();
                     bookServiceImpl.create(author, title, isbn, date);
                 }
--- src/main/java/com/zinko/data/dao/connection/ConnectionContext.java
+++ src/main/java/com/zinko/data/dao/connection/ConnectionContext.java
@@ -1,6 +1,5 @@
 package com.zinko.data.dao.connection;
 
-import javax.management.DescriptorRead;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,12 +8,12 @@ public class ConnectionContext {
     public static final String POSTGRES_URL = "jdbc:postgresql://localhost:5432/bookstore_bh";
     public static final String POSTGRES_USER = "postgres";
     public static final String POSTGRES_PASSWORD = "root";
-    public static final String ELEPHANT_URL = "jdbc:postgresql//flora.db.elephantsql.com:5432/gaemuhia";
-    public static final String ELEPHANT_USER = "gaemuhia";
-    public static final String  ELEPHANT_PASSWORD = "YcYoJ0CWFoir3Fwh6xX85q_8tVOWm1fA";
+    public static final String ELEPHANT_URL = "jdbc:postgresql://surus.db.elephantsql.com:5432/tsojyrly";
+    public static final String ELEPHANT_USER = "tsojyrly";
+    public static final String  ELEPHANT_PASSWORD = "ib1fM1-UprYuJxNAKvpQNdMwfEb8Z7qn";
 
     public static final Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(POSTGRES_URL, POSTGRES_USER, POSTGRES_PASSWORD);
-//        return DriverManager.getConnection(ELEPHANT_URL, ELEPHANT_USER, ELEPHANT_PASSWORD);
+//        return DriverManager.getConnection(POSTGRES_URL, POSTGRES_USER, POSTGRES_PASSWORD);
+        return DriverManager.getConnection(ELEPHANT_URL, ELEPHANT_USER, ELEPHANT_PASSWORD);
     }
 }

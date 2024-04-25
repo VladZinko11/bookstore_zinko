88de2432ac27f5a3887ed69e5f938d47c1566e3c
VladZinko <zinko1996@yandex.ru>
2024-04-25 18:05:53 +0300

    created methods in interface UserDao:
    create, findById, findAll, update, delete, findByEmail, findByLastName, countAll

--- src/main/java/com/zinko/data/dao/UserDao.java
+++ src/main/java/com/zinko/data/dao/UserDao.java
@@ -1,4 +1,24 @@
 package com.zinko.data.dao;
 
+import com.zinko.data.dao.entity.User;
+
+import java.util.List;
+
 public interface UserDao {
+
+    boolean create(User user);
+
+    User findById(Long id);
+
+    List<User> findAll();
+
+    boolean update();
+
+    boolean delete();
+
+    User findByEmail(String email);
+
+    List<User> findByLastName(String LastName);
+
+    Long countAll();
 }
